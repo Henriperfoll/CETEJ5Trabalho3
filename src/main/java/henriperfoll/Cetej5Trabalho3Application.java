3384cc535432f5bce9591ca6bdcabe2dbaf3d62d
Henrique Perfoll Neto <hperfoll@gmail.com>
2019-08-09 08:23:04 -0300

    first commit

---
+++ src/main/java/henriperfoll/Cetej5Trabalho3Application.java
@@ -0,0 +1,13 @@
+package henriperfoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cetej5Trabalho3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cetej5Trabalho3Application.class, args);
+	}
+
+}
---
+++ src/test/java/henriperfoll/Cetej5Trabalho3ApplicationTests.java
@@ -0,0 +1,16 @@
+package henriperfoll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cetej5Trabalho3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
